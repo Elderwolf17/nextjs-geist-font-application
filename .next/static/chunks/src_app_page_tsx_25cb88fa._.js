f85584d14b2f1087f333b2fa95f626a7ae366423
ELDER WOLFGRAMM MOREIRA <34742907+Elderwolf17@users.noreply.github.com>
2025-08-14 11:19:53 -0300

    Add .next/static/chunks/src_app_page_tsx_25cb88fa._.js 1755181191896

---
+++ .next/static/chunks/src_app_page_tsx_25cb88fa._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_19b7ce47._.js",
+  "static/chunks/node_modules_46cd5de3._.js"
+],
+    source: "dynamic"
+});
